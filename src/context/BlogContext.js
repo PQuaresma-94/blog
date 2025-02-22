002a21857e90bd34aa2fb063408b819df319665c
PQuaresma-94 <pedromquaresma@gmail.com>
2025-02-21 17:03:21 -0800

    Rendering a List of Props

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,7 +3,15 @@ import React, { Children } from "react";
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider value={123}>{children}</BlogContext.Provider>;
+  const blogPost = [
+    { title: "Blog Post #1" },
+    { title: "Blog Post #2" },
+    { title: "Blog Post #3" },
+  ];
+
+  return (
+    <BlogContext.Provider value={blogPost}>{children}</BlogContext.Provider>
+  );
 };
 
 export default BlogContext;
