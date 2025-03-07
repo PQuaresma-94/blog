6148f345ceb1536e55742d61cf00ec23c2186278
PQuaresma-94 <pedromquaresma@gmail.com>
2025-03-07 15:13:55 -0800

    Updated CreateScreen component

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,14 +1,54 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useContext } from "react";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
 
 const CreateScreen = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
   return (
-    <View>
-      <Text>Create Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Create a new Blog</Text>
+      <Text style={styles.label}>Enter Title:</Text>
+      <TextInput
+        value={title}
+        onChangeText={(text) => setTitle(text)}
+        style={styles.input}
+      />
+      <Text style={styles.label}>Enter Content:</Text>
+
+      <TextInput
+        value={content}
+        onChangeText={(text) => setContent(text)}
+        style={styles.input}
+      />
+
+      <Button
+        title="Add Blog Post"
+        onPress={() => console.log({ title, content })}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+  },
+  title: {
+    fontSize: 24,
+    alignSelf: "center",
+    marginBottom: 10,
+  },
+  label: {
+    fontSize: 20,
+  },
+  input: {
+    fontSize: 18,
+    borderRadius: 10,
+    borderWidth: 2,
+    padding: 10,
+    marginVertical: 5,
+  },
+});
 
 export default CreateScreen;
